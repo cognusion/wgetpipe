5e912cba5cdcc6beff349c2291bc3a8cc6529497
M <m@cognusion.com>
2025-08-14 12:19:54 -0400

    Refactors execution stream to be more grinder-like.
    Fixes defects around multiple Transports. *facepalm*

--- wgetpipe.go
+++ wgetpipe.go
@@ -28,6 +28,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -60,6 +61,16 @@ type urlCode struct {
 	Size int64
 	Dur  time.Duration
 	Err  error
+}
+
+// written to exclusively by collate().
+// Unsafe to read from until rChan is closed.
+type stat struct {
+	count      int
+	error4s    int
+	error5s    int
+	errors     int
+	mismatches int
 }
 
 func init() {
@@ -100,6 +111,7 @@ func init() {
 
 		rt, _ := rangetripper.NewWithLoggers(10, nil, DebugOut)
 		rt.SetChunkSize(chunkSize)
+		rt.SetClient(http.DefaultClient)
 		Client.Transport = rt
 	}
 
@@ -119,98 +131,115 @@ func init() {
 
 func main() {
 
-	var bar *pb.ProgressBar
-	getChan := make(chan string, MaxRequests*10) // Channel to stream URLs to get
-	rChan := make(chan urlCode)                  // Channel to stream responses from the Gets
-	doneChan := make(chan bool)                  // Channel to signal a getter is done
-	abortChan := make(chan bool)                 // Channel to tell the getters to abort
-	count := 0
-	error4s := 0
-	error5s := 0
-	errors := 0
+	var (
+		bar       *pb.ProgressBar
+		rStats    stat
+		wg        sync.WaitGroup
+		gwg       sync.WaitGroup
+		getChan   = make(chan string, MaxRequests*10) // Channel to stream URLs to get
+		rChan     = make(chan urlCode)                // Channel to stream responses from the Gets
+		abortChan = make(chan bool)                   // Channel to tell the getters to abort
+	)
 
 	// Set up the progress bar
 	if useBar {
+		color.Output = io.Discard // disable colorized output
 		tmpl := `{{string . "prefix"}}{{counters . }} {{bar . }} {{percent . }} {{rtime . "ETA %s"}}{{string . "suffix"}}`
 		bar = pb.ProgressBarTemplate(tmpl).New(totalGuess)
+		bar.Start()
 	}
 
 	sigDone := signalhandler.Simple(func(os.Signal) { close(abortChan) })
 	defer sigDone()
 
 	// Spawn off the getters
+	gwg.Add(MaxRequests)
 	for range MaxRequests {
-		go getter(getChan, rChan, doneChan, abortChan, timeout)
+		go getter(getChan, rChan, abortChan, timeout, &gwg)
 	}
 
-	// Block until all the getters are done, and then close rChan
-	go func() {
-		defer close(rChan)
-
-		for c := range MaxRequests {
-			<-doneChan
-			DebugOut.Printf("Done %d/%d\n", c+1, MaxRequests)
-		}
-	}()
+	// Hire a collator to collate the results
+	wg.Add(1)
+	go collate(&rStats, abortChan, rChan, &wg, bar)
 
 	// spawn off the scanner
 	start := time.Now()
-	go scanStdIn(getChan, abortChan, bar)
+	wg.Add(1)
+	go scanStdIn(getChan, abortChan, &wg, bar)
 
+	gwg.Wait() // Wait for getters
+	close(rChan)
+	wg.Wait() // Wait for processors
+
+	elapsed := time.Since(start)
 	if useBar {
-		bar.Start()
+		bar.Finish()
 	}
-	// Collate the results
-	for i := range rChan {
-		count++
 
-		if useBar {
+	if Summary {
+		color.Output = os.Stdout // re-enable this
+		e := color.RedString("%d", rStats.errors)
+		e4 := color.YellowString("%d", rStats.error4s)
+		e5 := color.RedString("%d", rStats.error5s)
+		eX := color.RedString("%d", rStats.mismatches)
+		fmt.Printf("\n\nGETs: %d\nErrors: %s\n500 Errors: %s\n400 Errors: %s\nMismatches: %s\nElapsed Time: %s\n", rStats.count, e, e5, e4, eX, elapsed.String())
+	}
+
+}
+
+func collate(stats *stat, abortChan chan bool, rChan chan urlCode, wg *sync.WaitGroup, bar *pb.ProgressBar) {
+	defer DebugOut.Println("collate complete")
+	defer wg.Done()
+
+	var zu = urlCode{}
+	for i := range rChan {
+		if i == zu {
+			// zero value means we're done
+			return
+		}
+
+		// check for
+		select {
+		case <-abortChan:
+			// Aborting
+			DebugOut.Println("collate aborting!")
+			return
+		default:
+		}
+
+		stats.count++
+
+		if bar != nil {
 			bar.Increment()
 		}
+
 		if i.Code == 0 {
-			errors++
-			if useBar {
-				continue
-			}
+			stats.errors++
 			color.Red("%d (%s) %s %s (%s)\n", i.Code, humanity.ByteFormat(i.Size), i.URL, i.Dur.String(), i.Err)
 		} else if i.Code < 400 {
-			if ErrOnly || useBar {
+			if ErrOnly {
 				// skip
 				continue
 			}
 			color.Green("%d (%s) %s %s\n", i.Code, humanity.ByteFormat(i.Size), i.URL, i.Dur.String())
 		} else if i.Code < 500 {
-			error4s++
-			if useBar {
-				continue
-			}
+			stats.error4s++
 			color.Yellow("%d (%s) %s %s\n", i.Code, humanity.ByteFormat(i.Size), i.URL, i.Dur.String())
+		} else if i.Code < 600 {
+			stats.error5s++
+			color.Red("%d (%s) %s %s\n", i.Code, humanity.ByteFormat(i.Size), i.URL, i.Dur.String())
 		} else {
-			error5s++
-			if useBar {
-				continue
-			}
+			stats.mismatches++
 			color.Red("%d (%s) %s %s\n", i.Code, humanity.ByteFormat(i.Size), i.URL, i.Dur.String())
 		}
-	}
-
-	if useBar {
-		bar.Finish()
-	}
-	elapsed := time.Since(start)
-
-	if Summary {
-		e := color.RedString("%d", errors)
-		e4 := color.YellowString("%d", error4s)
-		e5 := color.RedString("%d", error5s)
-		fmt.Printf("\n\nGETs: %d\nErrors: %s\n500 Errors: %s\n400 Errors: %s\nElapsed Time: %s\n", count, e, e5, e4, elapsed.String())
 	}
 }
 
 // scanStdIn takes a channel to pass inputted strings to,
 // and does so until EOF, whereafter it closes the channel
-func scanStdIn(getChan chan string, abortChan chan bool, bar *pb.ProgressBar) {
+func scanStdIn(getChan chan string, abortChan chan bool, wg *sync.WaitGroup, bar *pb.ProgressBar) {
 	defer close(getChan)
+	defer wg.Done()
 
 	scanner := bufio.NewScanner(os.Stdin)
 	count := int64(0)
@@ -241,8 +270,8 @@ func scanStdIn(getChan chan string, abortChan chan bool, bar *pb.ProgressBar) {
 // running HTTP GETs for anything in the receive channel, returning
 // formatted responses to the send channel, and signalling completion
 // via the done channel
-func getter(getChan chan string, rChan chan urlCode, doneChan chan bool, abortChan chan bool, timeout time.Duration) {
-	defer func() { doneChan <- true }()
+func getter(getChan chan string, rChan chan urlCode, abortChan chan bool, timeout time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
 
 	var (
 		ctx    context.Context
@@ -305,14 +334,18 @@ func getter(getChan chan string, rChan chan urlCode, doneChan chan bool, abortCh
 				}
 
 				if Save {
-					SaveFile(url, &b)
+					if err := SaveFile(url, &b); err != nil {
+						fmt.Printf("Error saving response: '%s'\n", err)
+					}
 				}
 			} else if Save {
 				b, err := io.ReadAll(response.Body)
 				if err != nil {
 					fmt.Printf("Error reading response body: '%s' not saving file '%s'\n", err, url)
 				} else {
-					SaveFile(url, &b)
+					if err := SaveFile(url, &b); err != nil {
+						fmt.Printf("Error saving response: '%s'\n", err)
+					}
 				}
 			}
 			rChan <- urlCode{url, response.StatusCode, response.ContentLength, d, nil}
@@ -349,12 +382,12 @@ func SaveFile(saveAs string, contents *[]byte) error {
 	}
 
 	DebugOut.Printf("Saved File Path: '%s%s' full: '%s%s'\n", url.Hostname(), dirs, url.Hostname(), url.Path)
-	err = os.MkdirAll(fmt.Sprintf("%s%s", url.Hostname(), dirs), os.ModePerm)
+	err = os.MkdirAll(fmt.Sprintf("%s%s", url.Hostname(), dirs), 0750)
 	if err != nil {
 		return err
 	}
 
-	err = os.WriteFile(fmt.Sprintf("%s%s", url.Hostname(), url.Path), *contents, os.ModePerm)
+	err = os.WriteFile(fmt.Sprintf("%s%s", url.Hostname(), url.Path), *contents, 0600)
 	if err != nil {
 		return err
 	}
